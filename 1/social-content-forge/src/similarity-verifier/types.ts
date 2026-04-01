535556c035a4baaff452d065ae1d5f797d07ac9c
mark <xpyy125@gmail.com>
2026-04-01 17:49:04 +0800

    feat(similarity-verifier): create types module

---
+++ 1/social-content-forge/src/similarity-verifier/types.ts
@@ -0,0 +1,15 @@
+import type {
+  SimilarityResult,
+  DimensionResult,
+  SimilarityThresholds,
+  DEFAULT_SIMILARITY_THRESHOLDS,
+  SIMILARITY_DIMENSION_WEIGHTS,
+} from '../types';
+
+export {
+  type SimilarityResult,
+  type DimensionResult,
+  type SimilarityThresholds,
+  DEFAULT_SIMILARITY_THRESHOLDS,
+  SIMILARITY_DIMENSION_WEIGHTS,
+};
